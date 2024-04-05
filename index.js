dfb058d3ee1ffd79639f0e1c962dccd99cd69108
Mark Mwiti <muthamiamark@gmail.com>
2024-04-05 10:36:20 +0300

    Adds lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+
+console.log("The company name is:", companyName);
+
+function mostProfitableNeighborhood() {
+    let mostProfitableNeighborhood = 'Chelsea'; 
+    return mostProfitableNeighborhood;
+  }
+
+function companyCeo() {
+    let companyCeo = 'Susan Smith';
+    return companyCeo;
+}
